5a2a0790168a69cd16709b11df234385c6571aa3
Long Nguyen <89681530+Evolting@users.noreply.github.com>
2023-02-25 21:35:29 +0700

    2023/02/25 LightCMS Project initiation

---
+++ FPT_CMS/LightCMS/ViewComponents/NavbarViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Principal;
+
+namespace LightCMS.ViewComponents
+{
+    [ViewComponent]
+    public class NavbarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ FPT_CMS/LightCMS/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LightCMS.ViewComponents
+{
+    [ViewComponent]
+    public class SidebarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
