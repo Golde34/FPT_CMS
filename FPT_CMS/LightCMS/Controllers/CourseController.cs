ed02109421219a78b5e52a5f5b3c70dcc2a90e2b
Long Nguyen <89681530+Evolting@users.noreply.github.com>
2023-03-04 20:11:02 +0700

    2023/03/04 tạo màn hình List Course

---
+++ FPT_CMS/LightCMS/Controllers/CourseController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace LightCMS.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly HttpClient client = null;
+        private string CmsApiUrl = "";
+
+        public CourseController()
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            CmsApiUrl = "http://localhost:5195/api/Courses";
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // GET JWT AND END IT ALONG WITH THE REQUEST
+            if (HttpContext.Session.GetString("JWT") != null)
+            {
+                var token = HttpContext.Session.GetString("JWT").Replace('"', ' ').Trim();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.ToString());
+            }
+            HttpResponseMessage response = await client.GetAsync(CmsApiUrl+"/GetCourses");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            //string strData = await response.Content.ReadAsStringAsync();
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
---
+++ FPT_CMS/Server/Controllers/CoursesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CoursesController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetCourses()
+        {
+            return Ok();
+        }
+    }
+}
